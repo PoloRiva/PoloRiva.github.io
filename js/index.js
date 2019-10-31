599c54348c930cfc67afa9db2e886d3a2394bba2
PoloRiva <riva.polo@gmail.com>
2019-10-31 18:38:52 +0100

    smooth collapse

--- js/index.js
+++ js/index.js
@@ -5,3 +5,11 @@
 // window.onscroll = function() {
 //     console.log('Hi');
 // }
+
+// var a = document.getElementsByClassName("bg-white rounded shadow bg-white rounded");
+// console.log(a);
+// $(document.getElementsByClassName("bg-white rounded shadow bg-white rounded")).on('hide.bs.collapse', myFunction());
+// function myFunction() {
+//     console.log('Hi');
+// }
+
