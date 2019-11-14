1c90d19cbc1046893306d1216e233c60ece09752
PoloRiva <riva.polo@gmail.com>
2019-11-14 17:23:58 +0100

    added some -still broken- changing style to the download btn on Resume

--- js/index.js
+++ js/index.js
@@ -1,34 +1,10 @@
-// $(function() {
-//     $("#nav-placeholder").load("navbar.html");
-// });
-
-// window.onscroll = function() {
-//     console.log('Hi');
-// }
-
-// var a = document.getElementsByClassName("bg-white rounded shadow bg-white rounded");
-// console.log(a);
-// $(document.getElementsByClassName("bg-white rounded shadow bg-white rounded")).on('hide.bs.collapse', myFunction());
-// function myFunction() {
-//     console.log('Hi');
-// }
-
-$(document).ready(function () {
-    var url = window.location;
-    var pos = window.p
-    $('ul.navbar-nav a[href=' + url + '"]').parent().addClass('active');
-    $('ul.navbar-nav a').filter(function() {
-        return this.href == url;
-    }).parent.addClass('active');
-});
-
-//This functions are useful to remove and add classes to elements
+// ---------- This functions are useful to remove and add classes to elements ----------
 // $( '#topheader .navbar-nav a' ).on( 'click', function () {
 // 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 // 	$( this ).parent( 'li' ).addClass( 'active' );
 // });
 
-// 2. For the "Back to Top" button
+// ---------- For the "Back to Top" button ----------
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
@@ -47,4 +23,19 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// ---------- For the button for downloading the CV ----------
+//btn-outline-primary
+// TODO: change "onmouseover" to enter and do the reverse action for exiting
+var myDownBtn = document.getElementById("DownloadBtn");
+
+myDownBtn.onmouseover = function() {
+    changeClassBtn();
+}
+
+function changeClassBtn() {
+    myDownBtn.classList.remove("btn-primary");
+    myDownBtn.classList.add("btn-outline-primary");
+
 }
