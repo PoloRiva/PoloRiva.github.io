efb9cce10451bc9f88847c3fdacabeed624260ad
PoloRiva <riva.polo@gmail.com>
2019-11-27 10:25:06 +0100

    added the Four columns to cell tool

--- js/AccentureTools.js
+++ js/AccentureTools.js
@@ -1,4 +1,5 @@
 
+// Single column to array
 function toList() {
     var text = document.getElementById("toList0").value;
 
@@ -19,4 +20,26 @@ function toList() {
         document.getElementById("toList1").select();
         document.execCommand("copy");
     }
+};
+
+// Four columns to cell
+function toCell() {
+    var text = document.getElementById("toCell0").value;
+
+    var allLines = text.split("    ");
+    console.log(allLines);
+    solution = "";
+    solution = solution.concat("entityID: " + allLines[0] + " ");
+    for (i=0; i<((allLines.length-1)/3); i++) { //this runs once for every line in the input
+        console.log(allLines.length);
+        solution = solution.concat("id_ndb: " + allLines[2+3*i] + " ");
+        solution = solution.concat("id_uim: " + allLines[3+3*i].split(/\r\n|\n/)[0] + "\n");
+        // FIXME: if the last cell has a value in the "1" position after the split
+        if(allLines[3+3*i].split(/\r\n|\n/)[1]!==undefined) {
+            console.log(allLines[3+3*i].split(/\r\n|\n/)[1]);
+            solution = solution.concat("entityID: " + allLines[3+3*i].split(/\r\n|\n/)[1] + " ");
+        }
+        console.log(allLines[3+3*i].split(/\r\n|\n/));
+    }
+    document.getElementById("toCell1").value = solution;
 };
